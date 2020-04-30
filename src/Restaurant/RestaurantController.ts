4e1b7c503fd529a0f701c57ddd7b8febf6662cb4
mattig3090 <36996707+mattig3090@users.noreply.github.com>
2020-04-30 02:54:12 -0400

    Added basic post and delete methods to models

--- src/Restaurant/RestaurantController.ts
+++ src/Restaurant/RestaurantController.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { request } from "http";
+import { RestaurantModel } from './RestaurantModel';
 import { Database } from '../common/MongoDB';
 import { Config } from '../config';
 
 export class RestaurantController {
-    static db: Database = new Database(Config.url, "projects");
+    static db: Database = new Database(Config.url, "restaurant");
+    static restaurantTable = 'restaurant';
 
     public getRestaurant(req: express.Request, res: express.Response): void {
         console.log(req.params.id);
@@ -12,13 +14,20 @@ export class RestaurantController {
         console.log("output");
 
     }
-    public postRestaurant(req: express.Request, res: express.Response): void {
-        console.log("name");
-        res.send(req.body.name);
+    public postRestaurant(req: express.Request, res: express.Response){
+        const resta: RestaurantModel = RestaurantModel.fromObject(req.body);
+
+        RestaurantController.db.addRecord(RestaurantController.restaurantTable, resta.toObject())
+            .then((result: boolean) => res.send({fn: 'addRestaurant', status: 'success'}).end())
+            .catch((reason) => res.status(500).send(reason).end());
     }
 
     public deleteRestaurant(req: express.Request, res: express.Response): void{
-        console.log("delete event: "+ req.params.id);
+        const id = Database.stringToId(req.params.id);
+
+        RestaurantController.db.deleteRecord(RestaurantController.restaurantTable, {_id: id})
+            .then((results) => results ? (res.send({fn: 'deleteRestaurant', status: 'success' })) : (res.send({fn: 'deleteRestaurant', status: 'failure', data: 'Not found' })).end())
+            .catch((reason) => res.status(500).send(reason).end());
     }
 
     public getAllRestaurants(req: express.Request, res: express.Response): void {
--- src/User/UsersController.ts
+++ src/User/UsersController.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { request } from "http";
+import { UserModel } from './UserModel';
+import { Database } from '../common/MongoDB';
+import { Config } from '../config';
 
-export class UsersController {
+export class UserController {
+    static db: Database = new Database(Config.url, 'user');
+    static userTable = 'user';
 
     public getUsers(req: express.Request, res: express.Response): void {
         console.log(req.params.id);
@@ -10,12 +15,19 @@ export class UsersController {
 
     }
     public postUsers(req: express.Request, res: express.Response): void {
-        console.log("name");
-        res.send(req.body.name);
+        const use: UserModel = UserModel.fromObject(req.body);
+
+        UserController.db.addRecord(UserController.userTable, use.toObject())
+            .then((result: boolean) => res.send({fn: 'addRestaurant', status: 'success'}).end())
+            .catch((reason) => res.status(500).send(reason).end());
     }
 
     public deleteUsers(req: express.Request, res: express.Response): void{
-        console.log("delete event: "+ req.params.id);
+        const id = Database.stringToId(req.params.id);
+
+        UserController.db.deleteRecord(UserController.userTable, {_id: id})
+            .then((results) => results ? (res.send({fn: 'deleteRestaurant', status: 'success' })) : (res.send({fn: 'deleteRestaurant', status: 'failure', data: 'Not found' })).end())
+            .catch((reason) => res.status(500).send(reason).end());
     }
 
     public getAllUsers(req: express.Request, res: express.Response): void {
